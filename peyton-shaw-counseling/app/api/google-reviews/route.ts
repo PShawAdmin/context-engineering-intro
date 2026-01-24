c16e9aa07f73488e7ce0a69efc9e73260b6c90e6
PShawAdmin <admin@peytonshawcounseling.com>
2026-01-23 21:21:33 -0600

    Vercel Build Failure Fix

--- peyton-shaw-counseling/app/api/google-reviews/route.ts
+++ peyton-shaw-counseling/app/api/google-reviews/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export const revalidate = 60 * 60; // Cache for 1 hour
+export const revalidate = 3600; // Cache for 1 hour
 export const runtime = 'nodejs';
 
 type GooglePlacesLegacyResponse = {
